3e50c3cbb66e517a9034bc98effbd45a958f5912
BogdanRivera <bugy1998@hotmail.com>
2023-01-10 16:54:59 -0600

    Se agrego la librería de Pandas para leer el CSV Se filtraron los juegos

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+"""
+Este programa resuelve de una lista de juegos de Xbox aquellos que
+han sido jugados de 100 a 120 horas con Pandas
+"""
 import pandas as pd
+import graph 
 
 
 
+
+def run():
+    df = pd.read_csv("./DataXbox.csv")
+    df = df[df['TIME'] == '100-120 hours']
+    
+
+
+
+if __name__=="__main__":
+    run()
